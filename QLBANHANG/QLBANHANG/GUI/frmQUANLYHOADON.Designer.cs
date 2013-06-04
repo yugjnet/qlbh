4aed4e85ec10338f871f914401152f08c1df5ce3
yugjnet <yugjnet@gmail.com>
2013-06-05 02:36:38 +0700

    update quan ly hoa don

---
+++ QLBANHANG/QLBANHANG/GUI/frmQUANLYHOADON.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace QLBANHANG.GUI
+{
+    partial class frmQUANLYHOADON
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btFind = new System.Windows.Forms.Button();
+            this.dtFromDate = new System.Windows.Forms.DateTimePicker();
+            this.dtEndDate = new System.Windows.Forms.DateTimePicker();
+            this.dgBill = new System.Windows.Forms.DataGridView();
+            this.btClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgBill)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btFind
+            // 
+            this.btFind.Location = new System.Drawing.Point(505, 45);
+            this.btFind.Name = "btFind";
+            this.btFind.Size = new System.Drawing.Size(75, 23);
+            this.btFind.TabIndex = 0;
+            this.btFind.Text = "Tìm kiếm";
+            this.btFind.UseVisualStyleBackColor = true;
+            this.btFind.Click += new System.EventHandler(this.btFind_Click);
+            // 
+            // dtFromDate
+            // 
+            this.dtFromDate.Location = new System.Drawing.Point(41, 48);
+            this.dtFromDate.Name = "dtFromDate";
+            this.dtFromDate.Size = new System.Drawing.Size(191, 20);
+            this.dtFromDate.TabIndex = 1;
+            // 
+            // dtEndDate
+            // 
+            this.dtEndDate.Location = new System.Drawing.Point(254, 48);
+            this.dtEndDate.Name = "dtEndDate";
+            this.dtEndDate.Size = new System.Drawing.Size(200, 20);
+            this.dtEndDate.TabIndex = 2;
+            // 
+            // dgBill
+            // 
+            this.dgBill.AllowUserToOrderColumns = true;
+            this.dgBill.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgBill.Location = new System.Drawing.Point(41, 102);
+            this.dgBill.Name = "dgBill";
+            this.dgBill.Size = new System.Drawing.Size(644, 225);
+            this.dgBill.TabIndex = 3;
+            // 
+            // btClose
+            // 
+            this.btClose.Location = new System.Drawing.Point(610, 45);
+            this.btClose.Name = "btClose";
+            this.btClose.Size = new System.Drawing.Size(75, 23);
+            this.btClose.TabIndex = 4;
+            this.btClose.Text = "Thoát";
+            this.btClose.UseVisualStyleBackColor = true;
+            this.btClose.Click += new System.EventHandler(this.btClose_Click);
+            // 
+            // frmQUANLYHOADON
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 359);
+            this.Controls.Add(this.btClose);
+            this.Controls.Add(this.dgBill);
+            this.Controls.Add(this.dtEndDate);
+            this.Controls.Add(this.dtFromDate);
+            this.Controls.Add(this.btFind);
+            this.Name = "frmQUANLYHOADON";
+            this.Text = "frmQUANLYHOADON";
+            ((System.ComponentModel.ISupportInitialize)(this.dgBill)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btFind;
+        private System.Windows.Forms.DateTimePicker dtFromDate;
+        private System.Windows.Forms.DateTimePicker dtEndDate;
+        private System.Windows.Forms.DataGridView dgBill;
+        private System.Windows.Forms.Button btClose;
+    }
+}
---
+++ QLBANHANG/QLBANHANG/GUI/frmQUANLYHOADON.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace QLBANHANG.GUI
+{
+    public partial class frmQUANLYHOADON : Form
+    {
+        SqlConnection sc = new SqlConnection("Data Source=(local);Initial Catalog=qlbh;Persist Security Info=True;User ID=sa;Password=anhtuan");
+        public frmQUANLYHOADON()
+        {
+            InitializeComponent();
+        }
+
+        private void btFind_Click(object sender, EventArgs e)
+        {
+            sc.Open();
+            string query = "select HoaDon.MaHD, count(HoaDonChiTiet.MaHD) as TongSoMatHang, HoaDon.MaNV, HoaDon.TongTien,HoaDon.NgayTao from HoaDon, HoaDonChiTiet where HoaDon.MaHD = HoaDonChiTiet.MaHD and NgayTao between '" + dtFromDate.Value.ToString("yyyy-MM-dd") + "' and '" + dtEndDate.Value.ToString("yyyy-MM-dd") + "' group by HoaDon.MaHD, HoaDon.MaNV, HoaDon.TongTien, HoaDon.NgayTao";
+
+            SqlDataAdapter dataadapter = new SqlDataAdapter(query, sc);
+            DataSet ds = new DataSet();
+            dataadapter.Fill(ds, "HoaDon");
+
+            dgBill.DataSource = ds;
+            dgBill.DataMember = "HoaDon";
+            sc.Close();
+        }
+
+        private void btClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
